18b5124cf2c22e4ec75c235db966754b281e3b8c
Maxiboii <67202298+Maxiboii@users.noreply.github.com>
2020-09-10 21:47:08 +0300

    Linux fix

--- getLinks.py
+++ getLinks.py
@@ -10,7 +10,10 @@ class Google:
         self.password = password
 
     def login(self):
-        self.driver=webdriver.Chrome('/Users/Max/Desktop/Code/Selenium/chromedriver')
+        try:
+            self.driver=webdriver.Chrome('/Users/Max/Desktop/Code/Selenium/chromedriver')
+        except:
+            self.driver=webdriver.Chrome()
         self.driver.get('https://www.coursera.org/?authMode=login')
         original_window = self.driver.current_window_handle
         sleep(5)
